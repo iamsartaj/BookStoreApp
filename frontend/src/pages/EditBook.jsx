1f4bc8be1a1b69b1f1b6b6b102bdb38206e377fe
Sartaj Ahmad <138305054+iamsartaj@users.noreply.github.com>
2023-11-24 15:59:33 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -17,7 +17,7 @@ const EditBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`https://book-store-app-frontend.vercel.app/books/${id}`)
+      .get(`https://book-store-app-backend-khaki.vercel.app/books/${id}`)
       .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear);
@@ -39,7 +39,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`https://book-store-app-backend-khaki.vercel.app/book/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Edited successfully", { variant: "success" });
